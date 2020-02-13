e52723190290224323d30154e22cde1aceb87b7d
Michael Speiser <speiserm@protonmail.com>
2020-02-13 14:35:21 -0500

    added a bunch of functionality

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,9 +1,52 @@
 function store(field){
-     var field= document.getElementById(field);
-     localStorage.setItem(field, field.value);
-     testReplace();
-   }
+     var item = document.getElementById(field);
+     localStorage.setItem(field, item.value);
+}
 
-function testReplace() {
-  document.getElementById("testreplace").innerHTML = localStorage.getItem("wnumber");
+
+function makeHost(id) {
+  var host = new Host (id);
+  console.log('made it to makehost');
+}
+
+
+function Host (number) {
+  this.idNumber = number;
+  this.firstName = "Mike";
+  this.lastName = "Speiser";
+  this.getName = function () {return this.firstName + " " + this.lastName;};
+}
+
+function Person (number, first, last) {
+  this.idNumber = number;
+  this.firstName = first;
+  this.lastName = last;
+  this.getName = function () {return this.firstName + " " + this.lastName;};
+  this.overnight = new Boolean(false);
+  this.isOvernight = function () {return this.overnight;};
+}
+
+function increase (field) {
+  if(checkMaxGuests() && parseInt(document.getElementById(field).innerHTML) <= 3) {
+    document.getElementById(field).innerHTML = parseInt(document.getElementById(field).innerHTML)+1;
+  }
+}
+
+function decrease (field) {
+  if(parseInt(document.getElementById(field).innerHTML) >= 1) {
+    document.getElementById(field).innerHTML = parseInt(document.getElementById(field).innerHTML)-1;
+  }
+}
+
+function checkMaxGuests () {
+  if(((parseInt(document.getElementById('studentCount').innerHTML)) + (parseInt(document.getElementById('nonStudentCount').innerHTML)) >=4)) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+function fillPage () {
+  document.getElementById('nameheader').innerHTML += Host.getName;
 }
