8a2c78179b297775ec0aca784bae95f840ff80e8
Michael Speiser <speiserm@protonmail.com>
2020-02-17 23:58:35 -0500

    Did a bunch of functional work

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,29 +1,34 @@
-function store(field){
+let names = [
+  "Alissa Melsopp",
+  "Johnny Depp",
+  "Arnold Schwarzenegger",
+  "Jim Carrey",
+  "Emma Watson",
+  "Daniel Radcliffe",
+  "Leonardo DiCaprio",
+  "Tom Cruise",
+  "Brad Pitt",
+  "Charles Chaplin",
+  "Morgan Freeman",
+  "Tom Hanks",
+  "Hugh Jackman",
+  "Matt Damon",
+  "Sylvester Stallone",
+  "Will Smith",
+  "Clint Eastwood",
+  "Cameron Diaz",
+  "George Clooney",
+  "Stevel Spielberg",
+  "Harrison Ford",
+  "Robert De Niro",
+  "Al Pachino",
+  "Robert Downey Jr."
+];
+
+function store(field) {
      var item = document.getElementById(field);
      localStorage.setItem(field, item.value);
 }
-
-
-function makeHost(id) {
-  host = new Host (7);
-  console.log('made it to makehost');
-}
-
-
-var host = new Object();
-{
-  host.idNumber = 00000000;
-  host.firstName = "Mike";
-  host.lastName = "Speiser";
-  host.getName = getName(host.firstName, host.lastName);
-  host.numberguests = 0;
-}
-
-function getName (firstName, lastName) {
-  return firstName + " " + lastName;
-}
-
- var guests = {firstName:"John", lastName:"Doe", id:00000000}; 
 
 function increase (field) {
   if(checkMaxGuests() && parseInt(document.getElementById(field).innerHTML) <= 3) {
@@ -51,11 +56,42 @@ function fillNumberguests () {
   document.getElementById('nameheader').innerHTML += host.getName;
 }
 
+function getName() {
+  name = names[getRandomInt(names.length)];
+  return name;
+}
+
 function fillGuestinfo () {
-  for (var i = 0; i < names.length; i++) {
-    var name = names[i];
+  var list1 = document.getElementById('studentInfoList');
+  var list2 = document.getElementById('nonStudentInfoList');
+  for (var i = 0; i < localStorage.numberStudents; i++) {
+    var name = getName();
+    var textbox = document.createElement('input');
+    textbox.defaultValue = name;
+    var numberbox = document.createElement('input');
+    numberbox.class='wnumberInput';
+    numberbox.type='number';
+    numberbox.defaultValue=(getRandomInt(89999999)+10000000);
+    list1.appendChild(textbox);
+    list1.appendChild(numberbox);
+  }
+  for (var i = 0; i < localStorage.numberNonStudents; i++) {
+    var name = getName();
     var li = document.createElement('li');
     li.appendChild(document.createTextNode(name));
-    ul.appendChild(li);
+    list2.appendChild(li);
+  }
 }
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function saveGuestNumbers () {
+  localStorage.numberStudents  = parseInt(document.getElementById('studentCount').innerHTML);
+  localStorage.numberNonStudents = parseInt(document.getElementById('nonStudentCount').innerHTML);
+}
+
+function wipeGuests () {
+
 }
--- routes/numberguests.js
+++ routes/numberguests.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET wnumber page. */
 router.get('/', function (req, res) {
     res.render('numberguests', { title: 'Courtyard Kiosk',
-                                 prompt: 'How many guests are you checking in?'});
+                                 prompt: 'How many of your guests are students?'});
 });
 
 module.exports = router;
